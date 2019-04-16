c7c162f106a8956ff30bda31188a4f74aebcb16d
Biff Tannen <biffalo@protonmail.com>
2019-04-16 14:46:49 -0400

    removed a required button press and unused vars
    -removed line that required button press on this section which would cause script to hang against expectation/goal of being automated
    -removed 2 vars that were declared and then not used

--- admin-group-checker.ps1
+++ admin-group-checker.ps1
@@ -1,10 +1,6 @@
 ﻿$working = "C:\sec-audit"
 $domain = Get-ADDomain -Current LocalComputer | select -ExpandProperty name
-$da = "the above accounts are in the domain admin group"
-$a = "the above accounts are in the administrators group"
 Write-Host "We're now getting list of users that are in admin or domain admin groups for domain. Make sure there are no weird human accounts in here. Less privledge the better" -ForegroundColor Red -BackgroundColor White
-Write-Host -NoNewLine 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 Get-ADGroupMember "Domain Admins" | Select name >> $working\admin-report-$domain.txt
 "The above users are in the domain admin group" | out-file -append $working\admin-report-$domain.txt
 Get-ADGroupMember "Administrators" | Select name >> $working\admin-report-$domain.txt
