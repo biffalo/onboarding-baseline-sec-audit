2dc1dfc61f86cd98360d0cc395d6ff2d7b2ab313
Biff Tannen <biffalo@protonmail.com>
2019-05-28 08:55:33 -0400

    Additional Fix
    credit to rhobbs for rebuilding this.

--- office-365-have-i-been-pwned-check.ps1
+++ office-365-have-i-been-pwned-check.ps1
@@ -3,16 +3,13 @@ Install-Module MSOnline -force
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Connect-MsolService
-$UserAgent = “$((Get-MsolCompanyInformation).DisplayName) Security Audit”
-$baseUri = "https://haveibeenpwned.com/api/v2/"
-$users = Get-msoluser -All
+
+$UserAgent = “Infinity Technologies Security Audit”
+$users = Get-MsolUser -all
   
 foreach ($user in $users) {
-    $emails = $user.proxyaddresses | Where-Object {$_ -match "smtp:" -and $_ -notmatch ".onmicrosoft.com"}
-    $emails | ForEach-Object {
-        $email = ($_ -split ":")[1]
-        $uriEncodeEmail = [uri]::EscapeDataString($email)
-        $uri = "$baseUri/breachedaccount/$uriEncodeEmail"
+        $email = $user.UserPrincipalName
+        $uri = "https://haveibeenpwned.com/api/v2/breachedaccount/$email"
         $breachResult = $null
         try {
             [array]$breachResult = Invoke-RestMethod -Uri $uri -UserAgent $UserAgent -ErrorAction SilentlyContinue
@@ -21,9 +18,10 @@ foreach ($user in $users) {
             if($error[0].Exception.response.StatusCode -match "NotFound"){
                 Write-Host "No Breach detected for $email" -ForegroundColor Green
             }else{
-                Write-Host "Cannot retrieve results due to rate limiting or suspect IP. You may need to try a different computer"
+                Write-Host "$breachresult"
             }
         }
+        
         if ($breachResult) {
             foreach ($breach in $breachResult) {
                 $breachObject = [ordered]@{
@@ -32,15 +30,14 @@ foreach ($user in $users) {
                     BreachName         = $breach.Name
                     BreachTitle        = $breach.Title
                     BreachDate         = $breach.BreachDate
-                    BreachDataClasses  = ($breach.dataclasses -join ", ")
+                    BreachDataClasses  = ($breach.DataClasses -join ", ")
 
                 }
                 $breachObject = New-Object PSobject -Property $breachObject
                 $breachObject | Export-csv C:\sec-audit\BreachedAccounts.csv -NoTypeInformation -Append
-                Write-Host "Breach detected for $email - $($breach.name)" -ForegroundColor Yellow
+                Write-Host "Breach detected for $user - $($breach.name)" -ForegroundColor Yellow
                 Write-Host $breach.description -ForegroundColor DarkYellow
             }
         }
         Start-sleep -Milliseconds 2000
     }
-}
